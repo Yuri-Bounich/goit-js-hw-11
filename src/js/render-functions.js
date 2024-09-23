3ca0d681caf8832079b07470432e576fe0a55211
Yuri Bounich <b52ybunich@gmail.com>
2024-09-24 00:31:40 +0300

    hw1-1

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,7 +1,7 @@
-// Описаний у документації
-import iziToast from "izitoast";
-// Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+// // Описаний у документації
+// import iziToast from "izitoast";
+// // Додатковий імпорт стилів
+// import "izitoast/dist/css/iziToast.min.css";
 
 export const renderImages = images => {
     const gallery = document.querySelector('.gallery');
